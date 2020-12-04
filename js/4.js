78749cbf213065fe3ce5c0ca996d2662598f8284
tk338g <tkhazamov@wayfair.com>
2020-12-04 14:50:42 +0100

    Refactor. Day 4. JS

--- js/4.js
+++ js/4.js
@@ -2,7 +2,11 @@
 require("./solution")({
   input: () => require("./input").fetchFromAoC(4),
   solve(input) {
-    const lines = input.split("\n");
+    const lines = input
+      .split("\n\n")
+      .map((docLines) => docLines.split(/\s/))
+      .map((docPairs) => docPairs.map((pair) => pair.split(":")))
+      .map((/** @type [string, string][] */ docPairs) => new Map(docPairs));
 
     return [() => part1(lines), () => part2(lines)];
   },
@@ -19,50 +23,21 @@ const REQUIRED_FIELDS = Object.freeze([
 ]);
 
 /**
- * @param {string[]} lines
+ * @param {Map<string, string>[]} passports
  */
-function* getPassports(lines) {
-  let fields = new Map();
-  for (let line of lines) {
-    if (line === "") {
-      yield fields;
-      fields = new Map();
-    } else {
-      line.split(" ").forEach((pair) => {
-        const [key, value] = pair.split(":");
-        fields.set(key, value);
-      });
-    }
-  }
-  yield fields;
-}
-
-/**
- * @param {string[]} lines
- */
-function part1(lines) {
-  let validPassports = 0;
-
-  for (const passport of getPassports(lines)) {
-    validPassports += Number(
-      REQUIRED_FIELDS.every((field) => passport.has(field))
-    );
-  }
-
-  return validPassports;
+function part1(passports) {
+  return passports.filter((passport) =>
+    REQUIRED_FIELDS.every((field) => passport.has(field))
+  ).length;
 }
 
 const ECLs = new Set(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]);
 
 /**
  * @param {string} field
- * @param {Map<string, string>} passport
+ * @param {string} value
  */
-function isValidFieldInPassport(field, passport) {
-  if (!passport.has(field)) {
-    return false;
-  }
-  const value = passport.get(field);
+function isValidFieldInPassport(field, value) {
   switch (field) {
     case "byr": {
       return Number(value) >= 1920 && Number(value) <= 2002;
@@ -96,16 +71,14 @@ function isValidFieldInPassport(field, passport) {
 }
 
 /**
- * @param {string[]} lines
+ * @param {Map<string, string>[]} passports
  */
-function part2(lines) {
-  let validPassports = 0;
-
-  for (const passport of getPassports(lines)) {
-    validPassports += Number(
-      REQUIRED_FIELDS.every((field) => isValidFieldInPassport(field, passport))
-    );
-  }
-
-  return validPassports;
+function part2(passports) {
+  return passports.filter((passport) =>
+    REQUIRED_FIELDS.every(
+      (field) =>
+        passport.has(field) &&
+        isValidFieldInPassport(field, passport.get(field))
+    )
+  ).length;
 }
