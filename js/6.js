134ce709f99f4bb8a9893959f635215f5363de1b
tk338g <tkhazamov@wayfair.com>
2020-12-06 06:39:58 +0100

    Day 6. JS

---
+++ js/6.js
@@ -0,0 +1,35 @@
+// @ts-check
+require("./solution")({
+  input: () => require("./input").fetchFromAoC(6),
+  solve(input) {
+    const parsed = input.split("\n\n");
+
+    return [() => part1(parsed), () => part2(parsed)];
+  },
+});
+
+/**
+ * @param {string[]} groups
+ */
+function part1(groups) {
+  return groups
+    .map((g) => new Set(g.replace(/\s/g, "")).size)
+    .reduce((a, b) => a + b);
+}
+
+/**
+ * @param {string[]} groups
+ */
+function part2(groups) {
+  return groups
+    .map((g) => {
+      return g
+        .split("\n")
+        .map((line) => line.split(""))
+        .reduce((acc, line) => {
+          const memo = new Set(line);
+          return acc.filter((x) => memo.has(x));
+        }).length;
+    })
+    .reduce((a, b) => a + b);
+}
