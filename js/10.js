e356af71c20439a2a360280b56de91f70b9870ed
tk338g <tkhazamov@wayfair.com>
2020-12-10 10:38:18 +0100

    Day 10. JS
    Thanks Cyberpunk, ruined my score

---
+++ js/10.js
@@ -0,0 +1,72 @@
+// @ts-check
+
+require("./solution")({
+  input: () => require("./input").fetchFromAoC(10),
+  solve(input) {
+    return [() => part1(input), () => part2(input)];
+  },
+});
+
+/**
+ *
+ * @param {string} input
+ */
+function part1(input) {
+  const adapters = new Set(input.split("\n").map(Number));
+
+  const deviceAdapter = Math.max(...adapters) + 3;
+
+  adapters.add(deviceAdapter);
+
+  let cur = 0;
+  const frequences = { 1: 0, 2: 0, 3: 0 };
+  while (true) {
+    let hasAdapter = false;
+    for (let x of [1, 2, 3]) {
+      if (adapters.has(cur + x)) {
+        hasAdapter = true;
+        frequences[x]++;
+        cur += x;
+        break;
+      }
+    }
+    if (!hasAdapter) {
+      break;
+    }
+  }
+
+  return frequences[1] * frequences[3];
+}
+
+/**
+ * @param {string} input
+ */
+function part2(input) {
+  const adapters = new Set(input.split("\n").map(Number));
+  const deviceAdapter = Math.max(...adapters) + 3;
+  adapters.add(deviceAdapter);
+
+  const memo = new Map();
+
+  /**
+   * @param {number} joltage
+   */
+  function count(joltage) {
+    if (memo.has(joltage)) {
+      return memo.get(joltage);
+    }
+    if (joltage === deviceAdapter) {
+      return 1;
+    }
+    let result = 0;
+    for (let x of [1, 2, 3]) {
+      if (adapters.has(joltage + x)) {
+        result += count(joltage + x);
+      }
+    }
+    memo.set(joltage, result);
+    return result;
+  }
+
+  return count(0);
+}
