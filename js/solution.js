828f8caa383f6f33ffa8cd15cfcc8bc54467afda
tk338g <tkhazamov@wayfair.com>
2020-12-20 18:24:15 +0100

    Better output

--- js/solution.js
+++ js/solution.js
@@ -1,5 +1,13 @@
 // @ts-check
+const { performance } = require("perf_hooks");
 
+const runningFile = require("path").parse(process.argv[1]).name;
+
+const drawLine = (type) => {
+  let [l, r] = type === 1 ? ["╭", "╮"] : type === 2 ? ["╰", "╯"] : ["├", "┤"];
+  console.log(l + Array(38).fill("─").join("") + r);
+};
+const drawText = (text) => console.log(`│ ${text}`.padEnd(39, " ").concat("│"));
 /**
  * @param {Object} config
  * @param {() => string | Promise<string>} config.input
@@ -7,21 +15,29 @@
  */
 module.exports = async function solution({ input, solve }) {
   await Promise.resolve()
-    .then(input)
+    .then(() => {
+      drawLine(1);
+      drawText("Advent of Code. Day " + runningFile);
+      drawLine();
+      return input();
+    })
     .then(solve)
-    .then(
-      (solutions) => (
-        console.time(`Solution time`),
-        Promise.all(
-          solutions.map((solution, idx) =>
-            Promise.resolve()
-              .then(solution)
-              .then((result) => {
-                console.log(`Part ${idx + 1}: ${result}`);
-              })
-          )
-        ).then(() => console.timeEnd(`Solution time`))
-      )
+    .then((solutions) =>
+      solutions.reduce((acc, solution, idx) => {
+        let now = 0;
+        return acc
+          .then(() => {
+            now = performance.now();
+          })
+          .then(solution)
+          .then((result) => {
+            drawText(`Part ${idx + 1}`);
+            drawText("");
+            drawText(`Result: ${result}`);
+            drawText(`  Time: ${(performance.now() - now).toFixed(0)}ms`);
+            drawLine(idx === solutions.length - 1 ? 2 : 0);
+          });
+      }, Promise.resolve())
     )
     .catch(console.error);
 };
